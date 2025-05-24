2b9a8e0eb00cdd520bfcf5e9395abfd5e037a90c
Eric Lugo <@bu0115>
2025-05-24 03:03:49 +0200

    h

--- src/main/java/org/example/Dato/Jugadores/Jugador.java
+++ src/main/java/org/example/Dato/Jugadores/Jugador.java
@@ -1,19 +1,22 @@
 package org.example.Dato.Jugadores;
 
 
+import java.util.List;
+
 public abstract class Jugador implements IJugable {
 
-	public Jugador() throws Exception{
-	}
+	public Jugador() throws Exception {}
 
-	//Comprobar si esto se puede hacer????? aqui no, en el controlador si :)
-	public boolean existeEmail(String email){
-		return false;
-	}
-	public boolean comprobarEmailContrasenia(String email, String contraseña){
-		return false;
-	}
+	public abstract List<Integer> seleccionarCasilla();
 
-	
-	
+	public abstract String getId();
+
+	public abstract boolean decisionHabilidad();
+
+	public abstract int pedirFila();
+
+	public abstract boolean existeEmail(String email);
+
+	public abstract boolean comprobarEmailContrasenia(String email, String contraseña);
+
 }
